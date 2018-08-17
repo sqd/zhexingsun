cd07c4035964b27711640669297366be74a1aa82
sqd <hanyourmd@gmail.com>
2018-08-17 10:10:16 -0400

    add argparse options

--- local_proxy.py
+++ local_proxy.py
@@ -5,22 +5,19 @@ import os
 import socket
 import ssl
 import threading
-import gzip
-import zlib
 import time
 import re
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
-from cStringIO import StringIO
 from subprocess import Popen, PIPE
 import httplib
-import urlparse
+from argparse import ArgumentParser
 
-PROXY_SERVER_ADDRESS = 'localhost'
 PROXY_SERVER_SECRET_HEADER = 'X-Proxy-Secret'
-PROXY_SERVER_SECRET_KEY = '1234'
 PROXY_SERVER_SCHEME_HEADER = 'X-Proxy-Scheme'
 PROXY_SELF_SIGNED = True
+
+args = None
 
 
 def join_with_script_dir(path):
@@ -46,6 +43,7 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
     certkey = join_with_script_dir('certs/cert.key')
     certdir = join_with_script_dir('certs/')
     timeout = 5
+    protocol_version = "HTTP/1.1"
     lock = threading.Lock()
 
     def __init__(self, *args, **kwargs):
@@ -101,11 +99,11 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
             scheme = 'https' if isinstance(self.connection, ssl.SSLSocket) else 'http'
             path = req.path
             headers = dict(req.headers)
-            headers[PROXY_SERVER_SECRET_HEADER] = PROXY_SERVER_SECRET_KEY
+            headers[PROXY_SERVER_SECRET_HEADER] = args.password
             headers[PROXY_SERVER_SCHEME_HEADER] = scheme
 
-            conn = httplib.HTTPSConnection(PROXY_SERVER_ADDRESS, timeout=self.timeout,
-                                           context=ssl._create_unverified_context() if PROXY_SELF_SIGNED else None)
+            conn = httplib.HTTPSConnection(args.server, timeout=self.timeout,
+                                           context=ssl._create_unverified_context() if args.self_signed else None)
             conn.request(self.command, path, req_body, headers)
             res = conn.getresponse()
 
@@ -179,15 +177,20 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         pass
 
 
-def test(HandlerClass=ProxyRequestHandler, ServerClass=ThreadingHTTPServer, protocol="HTTP/1.1"):
-    if sys.argv[1:]:
-        port = int(sys.argv[1])
-    else:
-        port = 8080
-    server_address = ('', port)
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', help='port the local HTTP proxy listens on', type=int, default=8080)
+    parser.add_argument('-d', '--host', help='host the local HTTP proxy listens on', default='localhost')
+    parser.add_argument('-s', '--server', required=True, help='address of the remote proxy server')
+    parser.add_argument('-x', '--password', required=True, help='password')
+    parser.add_argument('-k', '--self-signed', action='store_true', help='[DANGEROUS] ignore SSL certification '
+                                                                         'warning when connecting to the proxy server')
+    global args
+    args = parser.parse_args()
 
-    HandlerClass.protocol_version = protocol
-    httpd = ServerClass(server_address, HandlerClass)
+    server_address = (args.host, args.port)
+
+    httpd = ThreadingHTTPServer(server_address, ProxyRequestHandler)
 
     sa = httpd.socket.getsockname()
     print "Serving HTTP Proxy on", sa[0], "port", sa[1], "..."
@@ -195,4 +198,4 @@ def test(HandlerClass=ProxyRequestHandler, ServerClass=ThreadingHTTPServer, prot
 
 
 if __name__ == '__main__':
-    test()
+    main()
